ce5b5e49bb9ed38446de2287f099fab23b9b6364
Gaurish-17 <104247521+Gaurish-17@users.noreply.github.com>
2022-04-24 14:48:53 +0530

    Create Students List.py

---
+++ Lab Assignment2/Students List.py
@@ -0,0 +1,8 @@
+Sid = int(input(("Please Enter Your SID:")))
+name = input("Please Enter Your Name:")
+gender = input("Please Enter Your Gender:")
+course_name = input("Please Enter Your Course Name:")
+cgpa = float(input("Please Enter Your CGPA:"))
+
+students_list = [Sid, name, gender, course_name, cgpa]
+print("Student" + str(students_list), '\n')
