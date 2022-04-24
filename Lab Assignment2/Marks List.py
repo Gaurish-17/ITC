db684919514fccfee55164b9ca863484bdc09cbf
Gaurish-17 <104247521+Gaurish-17@users.noreply.github.com>
2022-04-24 14:50:32 +0530

    Create Marks List.py

---
+++ Lab Assignment2/Marks List.py
@@ -0,0 +1,9 @@
+m1 = int(input("Enter your marks student 1:"))
+m2 = int(input("Enter your marks student 2:"))
+m3 = int(input("Enter your marks student 3:"))
+m4 = int(input("Enter your marks student 4:"))
+m5 = int(input("Enter your marks student 5:"))
+
+marksList = [m1, m2, m3, m4, m5]
+marksList.sort()
+print(marksList,'\n')
