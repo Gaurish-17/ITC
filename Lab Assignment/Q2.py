ece4428b87b36b3a459e5cf54be32fee2a016d36
Gaurish-17 <104247521+Gaurish-17@users.noreply.github.com>
2022-05-15 20:34:31 +0530

    Create Q2.py

---
+++ Lab Assignment/Q2.py
@@ -0,0 +1,10 @@
+# Variables
+Name = 'ABC'
+SID = '2110xxxx'
+Branch = 'XYZ'
+CGPA = '9.9'
+
+Intro = 'Hey, {} Here!\nMy SID is {}\nI am from {} Department and my CGPA is {}'
+
+# String Formatting
+print(Intro.format(Name,SID,Branch,CGPA))
