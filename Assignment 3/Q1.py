f33365de55eaac7424dc5af9d02b562689f115cb
Gaurish-17 <104247521+Gaurish-17@users.noreply.github.com>
2022-06-21 13:39:02 +0530

    Update Q1.py

--- Assignment 3/Q1.py
+++ Assignment 3/Q1.py
@@ -1,4 +1,11 @@
-n=int(input('Enter a number:'))
-n1= bin(n)
-n2=n1[2::]
-print(n2)
+num = int(input('Enter number: '))
+
+#Supposing we want to do this without bin() :
+a=''
+if num == 0:
+    print('Binary of number =', 0)
+else:
+    while num !=0:
+        a = str(num % 2) + a
+        num = num//2
+    print('Binary of number =', a)
